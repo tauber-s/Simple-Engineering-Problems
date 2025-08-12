b5671a5a1259513c7521228d41a39439df35e9dc
Sandy Tauber <63994145+tauber-s@users.noreply.github.com>
2025-08-11 22:42:00 -0300

    Create DPK03_impl_01.js

---
+++ src/DPK03_impl_01.js
@@ -0,0 +1,9 @@
+/*
+* DPK03 Lookup
+* Create a function that perform a lookup in a map for a given key you hould have id, name.
+* lookup(1) -> "John"
+* 
+* Refactor the code so you can lookup for email as well and ge the name and vice versa.
+* lookup("John") -> "john@john.jhon.com"
+* lookup("john@john.jhon.com") -> "John"
+*/
