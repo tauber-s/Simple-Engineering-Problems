bf83f6f3c4501be8fbfe7c0155f44502c8737129
Sandy Tauber <63994145+tauber-s@users.noreply.github.com>
2025-08-11 22:37:47 -0300

    Create DPK02_impl_02.js

---
+++ src/DPK02_impl_02.js
@@ -0,0 +1,11 @@
+/*
+* DPK02 Revert a List
+* Create a function that can revert a list.
+* revert([1,2,3,4,5]) -> [5,4,3,2,1]
+*/
+
+const revert = (numList) => {
+  return numList.reverse();
+};
+
+console.log(revert([1,2,3,4,5]));
