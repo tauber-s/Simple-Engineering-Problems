014ec6b4f94caa2e4b258c8005985ebdebb7b135
Sandy Tauber <63994145+tauber-s@users.noreply.github.com>
2025-08-11 22:23:25 -0300

    Update DPK01_impl_03.js

--- src/DPK01_impl_03.js
+++ src/DPK01_impl_03.js
@@ -1,1 +1,12 @@
 
+/*
+* DPK01 Revert String
+* Create a function that can revert a string.
+* revert("Hello") -> "olleH"
+*/
+
+const revert = (word) => {
+  return [...word].reverse().join('');
+};
+
+console.log(revert('Hello'));
