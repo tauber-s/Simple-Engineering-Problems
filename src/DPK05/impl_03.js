c1bcb71831e752dcce0d430cdd533b02e52ec785
Sandy <tauber.sandy@gmail.com>
2025-09-03 15:06:59 -0300

    Create impl 3,4

---
+++ src/DPK05/impl_03.js
@@ -0,0 +1,37 @@
+const power = {
+  "John": 100,
+  "Paul": 90,
+  "George": 80,
+  "Ringo": 70
+};
+
+let leaderboard = {
+  "John": 0,
+  "Paul": 0,
+  "George": 0,
+  "Ringo": 0
+};
+
+// should return the power of a given person
+const getPower = person => {
+  return power[person];
+};
+
+// should recive 2 names and the with one is the most powerful(should use function getPower)
+const mostPowerful = (p1, p2) => getPower(p1) > getPower(p2) ? p1 : p2;
+
+// should recive 2 names and update the leaderboard
+const play = (p1, p2) => {
+  const winner = mostPowerful(p1, p2);
+  if (getPower(p1) === getPower(p2)) {
+    [p1,p2].forEach(p => leaderboard[p] += 5);
+    return;
+  };
+  leaderboard[winner] += 10;
+  leaderboard[winner === p1 ? p2 : p1] -= 5;
+};
+
+play("John", "Paul");
+play("John", "Ringo");
+
+console.log(leaderboard);
