8fdce49d4a2f570527286dc0f0b44c64f773364e
Sandy Tauber <63994145+tauber-s@users.noreply.github.com>
2025-12-08 18:17:54 -0800

    add impl 3,4

---
+++ src/DPK14/impl_04.js
@@ -0,0 +1,32 @@
+const grid = [
+  ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+  ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+];
+
+const move = (grid, [row, col], moves) => {
+  const beaten = [];
+  const maxRow = grid.length - 1;
+  const maxCol = grid[0].length - 1;
+  let i=0;
+  do {
+    const walk=moves[i++];
+    let nextRow = row, nextCol = col
+    if(walk === "up") nextRow = row-1 < 0 ? maxRow : row-1;
+    if(walk === "down") nextRow = row+1 > maxRow ? 0 : row+1;
+    if(walk === "left") nextCol = col-1 < 0 ? maxCol : col-1;
+    if(walk === "right") nextCol = col+1 > maxCol ? 0 : col+1;
+    
+    if (grid[nextRow]?.[nextCol] !== undefined) {
+      grid[row][col] = "";
+      row = nextRow; 
+      col = nextCol;
+
+      if (grid[row][col]) 
+        beaten.push(grid[row][col]);
+      grid[row][col] = "";
+    }
+  } while(i<moves.length);
+  return beaten;
+};
+
+console.log(move(grid, [0,0], ["up", "left", "down", "right"]));
