561e14324652c18edfc99c9dcd9ec0a44295458a
Sandy Tauber <63994145+tauber-s@users.noreply.github.com>
2026-02-09 21:52:17 -0300

    Update Mosquito.js

--- src/DPK16/Mosquito.js
+++ src/DPK16/Mosquito.js
@@ -1,1 +1,14 @@
-export class Mosquito {};
+export class Mosquito {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.movesAlive = 0;
+    this.alive = true;
+  };
+
+  move() {
+    if (!this.alive) return;
+    
+    //const directions = ???
+  };
+};
